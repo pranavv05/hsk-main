3d6b503702f7f0a1b7b5623aff41a9eb010acac5
Pranav Vatsa <142565246+pranavv05@users.noreply.github.com>
2025-07-22 19:57:56 +0530

    Update apiService.ts

--- frontend/src/utils/apiService.ts
+++ frontend/src/utils/apiService.ts
@@ -2,7 +2,8 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
